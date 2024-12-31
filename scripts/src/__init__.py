ce05123188bb48f2060de378603f28f4dce78612
Kidyoh <kidyoh789@gmail.com>
2024-12-31 10:27:09 +0300

    init updated

---
+++ scripts/src/__init__.py
@@ -0,0 +1,6 @@
+# scripts/src/__init__.py
+
+# This file can be empty or can include package-level docstring
+"""
+This package contains source code for data processing, modeling, and utilities.
+"""
